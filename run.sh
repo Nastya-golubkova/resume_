947e94982207fe25a2e95646affdf2d0c84ae06a
Nastya-golubkova <122175283+Nastya-golubkova@users.noreply.github.com>
2023-06-18 20:37:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 git clone https://Nastya-golubkova@github.com/Nastya-golubkova/resume_
 cd resume_
 docker build -t pdflatex .
-docker run --rm -i -v pdflatex main.tex
+docker run --rm -i -v pdflatex -interaction=nonstopmode main.tex
