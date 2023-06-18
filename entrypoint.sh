ef80e0ae0a1ece989feb77becaaeb142e08ec50d
Nastya-golubkova <122175283+Nastya-golubkova@users.noreply.github.com>
2023-06-18 20:05:48 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://Nastya-golubkova@github.com/Nastya-golubkova/resume_
 cd resume_
-docker build -t Dockerfile
+docker build -t pdflatex .
 docker run --rm -i -v pdflatex main.tex
