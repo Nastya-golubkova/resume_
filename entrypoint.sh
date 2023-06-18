acb378424f087e7d8a1fc86cc9192c1aaaa5c5c3
Nastya-golubkova <122175283+Nastya-golubkova@users.noreply.github.com>
2023-06-18 19:57:13 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://Nastya-golubkova@github.com/Nastya-golubkova/resume_for_hw
+git clone https://Nastya-golubkova@github.com/Nastya-golubkova/resume_
 cd resume_for_hw
 docker build -t Dockerfile
 docker run --rm -i -v pdflatex main.tex
